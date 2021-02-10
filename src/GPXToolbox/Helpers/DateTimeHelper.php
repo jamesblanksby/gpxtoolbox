154e2b7b27a75b9ac81a2f24de2fed24f7cda1d2
James Blanksby <james@thisismyengine.com>
2021-02-10 12:08:19 +0000

    Fixed non static method

--- src/GPXToolbox/Helpers/DateTimeHelper.php
+++ src/GPXToolbox/Helpers/DateTimeHelper.php
@@ -11,7 +11,7 @@ class DateTimeHelper
      * @param string $timezone
      * @return string|null
      */
-    public function format($datetime, string $format = 'c', string $timezone = 'UTC') : ?string
+    public static function format($datetime, string $format = 'c', string $timezone = 'UTC') : ?string
     {
         if ($datetime instanceof \DateTime) {
             $datetime->setTimezone(new \DateTimeZone($timezone));
