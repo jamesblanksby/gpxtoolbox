6c60e414358984acfb9b6ed9adb39fcf1beb4419
James Blanksby <james@thisismyengine.com>
2023-02-14 14:41:59 +0200

    Updated example to include simple gpx dump

--- examples/01-get-started.php
+++ examples/01-get-started.php
@@ -7,6 +7,4 @@ include '../vendor/autoload.php';
 $toolbox = new GPXToolbox();
 $gpx = $toolbox::load('input.gpx');
 
-foreach ($gpx->trk as $trk) {
-    echo sprintf('<pre>%s</pre>', print_r($trk->toArray(), true));
-}
+echo sprintf('<pre>%s</pre>', print_r($gpx->toArray(), true));
