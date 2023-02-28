ef27968c1c6b3bc37d5c0c4705c09feab2e7e128
James Blanksby <james@thisismyengine.com>
2023-02-28 15:00:46 +0200

    Updated gpx file path

--- examples/01-get-started.php
+++ examples/01-get-started.php
@@ -5,6 +5,6 @@ use GPXToolbox\GPXToolbox;
 include '../vendor/autoload.php';
 
 $toolbox = new GPXToolbox();
-$gpx = $toolbox::load('input.gpx');
+$gpx = $toolbox::load(__DIR__ . '/input.gpx');
 
 echo sprintf('<pre>%s</pre>', print_r($gpx->toArray(), true));
